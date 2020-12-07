830d8b0fce959ca0d3df0655f2843059cdaea9b0
alexandra605 <70931822+alexandra605@users.noreply.github.com>
2020-12-07 21:19:07 +0200

    Add files via upload

---
+++ PR 4.py
@@ -0,0 +1,12 @@
+#problema 4 de la olimpiada cl a 9a
+#Pp, ap=perimetrul, latura, aria terenului patrat
+#ld, Ld=latimea, lungimea, aria terenului dreptunghic
+#A=aria
+Pp=48
+ap=Pp/4
+print("lungimea terenului patrat=",ap,"metri")
+A=ap**2
+print("aria terenurilor",A)
+ld=9
+Ld=A/ld
+print("lungimea terenului dreptunghic=",Ld,"metri")
